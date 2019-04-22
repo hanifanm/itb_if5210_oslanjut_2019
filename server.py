6bfcdb6a8e3193cf89cd1e9304b368236fcc8648
hanifanm <hanifanmohamad@gmail.com>
2019-04-22 22:53:00 +0700

    add javascript

--- server.py
+++ server.py
@@ -8,25 +8,38 @@ app = Flask(__name__, static_url_path='')
 def root():
     return app.send_static_file('index.html')
 
+def writeProgram(program, filename_full):
+    # WRITE TEMP PROGRAM
+    text_file = open(filename_full, "w+")
+    text_file.write(program)
+    text_file.close()
+
 @app.route('/compile', methods=['POST']) 
 def foo():
 
     # CREATE 30 CHAR RANDOM AS FILENAME
     filename = ''.join(random.choice('0123456789ABCDEF') for i in range(30))
-    filename_full = "program/" + filename + ".py"
     program = request.json['program']
+    language = request.json['lang']
 
-    # WRITE TEMP PROGRAM
-    text_file = open(filename_full, "w+")
-    text_file.write(program)
-    text_file.close()
-    
     # EXECUTE PROGRAM
-    cmd = ["python", filename_full]
+    if(language == 'python'):
+      filename_full = "program/" + filename + ".py"
+      writeProgram(program, filename_full)
+      cmd = ["python", filename_full]
+    elif(language == 'javascript'):
+      filename_full = "program/" + filename + ".js"
+      writeProgram(program, filename_full)
+      cmd = ["node", filename_full]
+    else:
+      cmd = ["echo", "language not supproted"]
+
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
     out,err = p.communicate()
+
+
     
     # RETURN
     return out
